ac73538810d33a31fbb5c368553bf9b417fc595a
Alexey Kashin <al.s.kashin@gmail.com>
2018-06-25 22:47:55 +0500

    рефакторинг экранов

--- modules/web/src/a/kashin/news/web/item/ItemBrowse.java
+++ modules/web/src/a/kashin/news/web/item/ItemBrowse.java
@@ -3,4 +3,14 @@ package a.kashin.news.web.item;
 import com.haulmont.cuba.gui.components.EntityCombinedScreen;
 
 public class ItemBrowse extends EntityCombinedScreen {
+
+    @Override
+    protected void initBrowseCreateAction() {
+
+    }
+
+    @Override
+    protected void initBrowseEditAction() {
+
+    }
 }
--- modules/web/src/a/kashin/news/web/site/SiteEdit.java
+++ modules/web/src/a/kashin/news/web/site/SiteEdit.java
@@ -1,20 +1,10 @@
 package a.kashin.news.web.site;
 
-import a.kashin.news.entity.Item;
-import a.kashin.news.service.NewsService;
-import com.haulmont.cuba.core.global.CommitContext;
-import com.haulmont.cuba.gui.components.AbstractEditor;
 import a.kashin.news.entity.Site;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import a.kashin.news.service.NewsService;
+import com.haulmont.cuba.gui.components.AbstractEditor;
 
 import javax.inject.Inject;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SiteEdit extends AbstractEditor<Site> {
     @Inject
